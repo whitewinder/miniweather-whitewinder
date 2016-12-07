19a6caab50362af64ba10befed3bb8c37fff79b5
wwh <971112338@qq.com>
2016-12-08 07:45:49 +0800

    the boot screen appears only at the first time

--- app/src/main/java/cn/edu/pku/ss/miniweather/miniweather/Welcome.java
+++ app/src/main/java/cn/edu/pku/ss/miniweather/miniweather/Welcome.java
@@ -1,7 +1,9 @@
 package cn.edu.pku.ss.miniweather.miniweather;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import java.util.Timer;
@@ -14,21 +16,46 @@ import cn.edu.pku.ss.miniweather.R;
  */
 //实现欢迎页面自动跳转到引导页面或主界面
 public class Welcome extends Activity {
+    private SharedPreferences preferences;//用于记录是不是第一次打开程序
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-        final Intent it=new Intent(this,Guide.class);
-        Timer timer=new Timer();
-        TimerTask task = new TimerTask(){
-            @Override
-            public void run() {
-                startActivity(it);
-            }
-        };
-        timer.schedule(task,1000*2);//2秒后自动跳转
-    }
+        preferences = getSharedPreferences("count", Context.MODE_PRIVATE);
+        int count = preferences.getInt("count", 0);
+        //判断程序与第几次运行，如果是第一次运行则跳转到引导页面
 
+        if (count == 0) {
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), Guide.class);
+                    startActivity(intent);
+                }
+            };
+            timer.schedule(task, 1000 * 2);//2秒后自动跳转
+        }
+        else{
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            };
+            timer.schedule(task, 1000 * 2);//2秒后自动跳转
+
+        }
+        editor = preferences.edit();
+        //存入数据
+        editor.putInt("count", ++count);
+        //提交修改
+        editor.commit();
+    }
 }
 
 
